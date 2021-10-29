dd644f98fa433290e2e93cab0b43cf6bfd8bebc3
spaghettimedic <dsteiner247@gmail.com>
2021-10-29 11:28:34 -0400

    initial setup of page - HTML and CSS

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var max = Math.max($("#option1").width(), $("#option2").width(), $("#option3").width(), $("#option4").width());
+$("#option1").width(max);
+$("#option2").width(max);
+$("#option3").width(max);
+$("#option4").width(max)
