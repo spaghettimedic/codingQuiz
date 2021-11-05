b8454d1cc2cb314bf7ed52290c1d32ca40a7a3e4
spaghettimedic <dsteiner247@gmail.com>
2021-11-05 14:58:37 -0400

    working on button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,34 +27,35 @@ var questions = [
 ];
 
 var quizContainerEl = document.querySelector("#quiz-container");
-var btnStart = document.querySelector("#btn-start");
-var header = document.querySelector("header");
-var btnOption = document.querySelector(".options");
+var btnStartEl = document.querySelector("#btn-start");
+var headerEl = document.querySelector("header");
+var timerEl = document.querySelector("#timer");
+var btnHighScoreEl = document.querySelector("#highScores");
 
 
-// function timer() {
-//     var sec = 75;
-//     var timer = 
-//         document.createElement("p");
-//         timer.className = "timer";
-//         timer.id = "timer";
-//         setInterval(function() {
-//             document.getElementById("timer").innerHTML = "Timer: " + sec;
-//             sec--;
-//             if (sec < 65) {
-//                 clearInterval(timer);
-//                 return alert("You ran out of time!");
-//             };
-//     });
-//     header.appendChild(timer);
-// };
+var sec = 75;
 
 
-var quizStart = function() {
-    document.getElementById("quiz-container").innerHTML = "";
+function timer() {
+    var timer = 
+        setInterval(function() {
+            timerEl.innerHTML = "Timer: " + sec;
+            sec--;
+            if (sec < 0) {
+                clearInterval(timer);
+                return alert("You ran out of time!");
+            };
+    }, 1000);
+};
 
-    for (var i = 0; i < questions.length; i++) {
+
+var queGenerator = function() {
+    
+    quizContainerEl.innerHTML = "";
+
+    for (let i = 0; i < questions.length; i++) {
         var question = questions[i].question;
+        var answer = questions[i].answer;
 
         // create a p element for every question
         var questionEl = document.createElement("p");
@@ -62,44 +63,104 @@ var quizStart = function() {
         questionEl.textContent = question;
         quizContainerEl.appendChild(questionEl);
 
-        for (var o = 0; o < questions[i].options.length; o++) {
-            var option = questions[i].options[o];
+        for (let o = 0; o < questions[i].options.length; o++) {
+            var option = questions[i].options;
 
             // create a button element for every option
-            var optionEl = document.createElement("button");
-            optionEl.classList = "options";
-            optionEl.textContent = option;
-            quizContainerEl.appendChild(optionEl);
+            var btnOptionEl1 = document.createElement("button");
+            btnOptionEl1.classList = "options";
+            btnOptionEl1.textContent = option[o];
+            quizContainerEl.appendChild(btnOptionEl1);
+
+            var optionSelected = document.querySelectorAll(".options");
+
+            // add event listener to option buttons and perform necessary actions based on user's choice
+            for (var b = 0; b < optionSelected.length; b++) {
+
+                // create a div to hold the answerConfirm message when an option is selected
+                var answerConfirmEl = document.createElement("div");
+                answerConfirmEl.classList = "answerConfirm";
+                answerConfirmEl.id = "answerConfirm";
+
+                if (this.textContent === answer) {
+                    optionSelected[b].addEventListener("click", function(){
+                        answerConfirmEl.innerHTML = "<br><hr><p><em>Correct!</em></p>"
+                        quizContainerEl.appendChild(answerConfirmEl);
+                        question++;
+                        option++;
+                        answer++;
+                        
+                        console.log("answerRight!");
+                    })
+                } else {
+                    optionSelected[b].addEventListener("click", function() {
+                        answerConfirmEl.innerHTML = "<br><hr><p><em>Wrong!</em></p>"
+                        quizContainerEl.appendChild(answerConfirmEl);
+                        question++;
+                        option++;
+                        answer++;
+
+                        sec = sec - 10;
+                        if (sec < 0) {
+                            sec = 0;
+                        };
+                        console.log("answerWrong!");
+                        console.log(answer);
+                    });
+                };
+            };
         };
     }
+    // capture timerScore when all questions have been answered
+    var timerScore = sec
+    handleScore();
 
-    var max = Math.max($("#option0").width(), $("#option1").width(), $("#option2").width(), $("#option3").width());
-    $("#option0").width(max);
-    $("#option1").width(max);
-    $("#option2").width(max);
-    $("#option3").width(max);
 };
 
-var checkAnswer = function(optionSelected) {
-    if (optionSelected === answer) {
-        var answerConfirmEl = document.createElement("div")
-        answerConfirmEl.classList = "answerConfirm";
-        answerConfirmEl.innerHTML = "<hr><p><em>Correct!</em></p>"
-        quizContainerEl.appendChild(answerConfirmEl);
-    } else {
-        var answerConfirmEl = document.createElement("div")
-        answerConfirmEl.classList = "answerConfirm";
-        answerConfirmEl.innerHTML = "<hr><p><em>Wrong!</em></p>"
-        quizContainerEl.appendChild(answerConfirmEl);
-    }
+
+// will store the current score if it is high enough in localStorage
+var handleScore = function() {
+
+    var userInfo = {
+        initials: "",
+        score: timerScore.value
+    };
+
+    quizContainerEl.innerHTML = "";
+
+    var yourScoreEl = document.createElement("div");
+    yourScoreEl.classList = "yourScore";
+    yourScoreEl.id = "yourScore";
+    yourScoreEl.innerHTML = "<form><p class='done'>All Done!</p>" +
+    "<p class='finalScore'>Your final score is " + userInfo.score + "</p>" +
+    "<label for='initials'>Enter initials: </label>" +
+    "<input id='userInput' type='text' placeholder='initials'>" +
+    "<button class='initialsSubmit'>Submit</button></form>";
+
+    userInfo.initials = document.getElementById("userInput").value;
+
+    localStorage.setItem("initials", userInfo.initials);
+    localStorage.setItem("score", userInfo.score);
 };
 
 // will access localStorage on button click
 function viewHighScores() {
+    quizContainerEl.innerHTML = "";
+    var highScoreEl = document.createElement("div");
+    highScoreEl.innerHTML = "<h2>High Scores</h2>" +
+    "<ul>" + "</ul>" +
+    "<br><button class='goBack' id='goBack'>Go Back</button>" +
+    "<button class='clearHighScores'>Clear High Scores</button>";
+    quizContainerEl.appendChild(highScoreEl);
+
+    var btnGoBackEl = document.getElementById("goBack");
 
 
-}
+    btnGoBackEl.addEventListener("click", function() {
+        window.location.reload();
+    });
+};
 
-btnStart.addEventListener("click", quizStart);
-btnStart.addEventListener("click", timer);
-btnOption.addEventListener("click", checkAnswer);
+btnHighScoreEl.addEventListener("click", viewHighScores);
+btnStartEl.addEventListener("click", queGenerator);
+btnStartEl.addEventListener("click", timer);
